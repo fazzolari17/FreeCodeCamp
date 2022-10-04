d69b228c02e0248f9cefa16f044604bc35e92817
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-10-04 09:00:12 -0400

    diff two arrays

---
+++ Javascript_Algorithms_and_Data_Structures/Intermediate_Algorithm_Scripting/diffTwoArrays.js
@@ -0,0 +1,7 @@
+function diffArray(arr1, arr2) {
+  return arr1.filter(x => !arr2.includes(x))
+    .concat(arr2.filter(x => !arr1.includes(x)));
+};
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
